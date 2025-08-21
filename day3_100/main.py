fd909a52a5e4179562c4f78506c9e6dc41ef2cfc
Harenabs21 <hei.harena.6@gmail.com>
2025-08-21 15:06:37 +0300

    feat: use commas in the print() function to concatenate variables with string

---
+++ day3_100/main.py
@@ -0,0 +1,13 @@
+print("=== Recipe Generator ===")
+print()
+print("Let's build an amazing dish for today's evening meal!")
+type_of_food = input("Name a food: ")
+type_of_plant = input("Name a type of plant: ")
+cooking_method = input("Name a method of cooking: ")
+word = input("What word describes burned food?")
+household_item = input("Name a household item: ")
+print()
+print("Menu")
+print("Tonight, we will be cooking", type_of_food, " with ", type_of_plant,
+  ". The method of cooking will be ", cooking_method, ". If it is ",
+  word, ", throw it in the ", household_item)
