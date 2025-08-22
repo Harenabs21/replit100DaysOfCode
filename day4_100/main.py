743ebe180663315a18eeaed5de4901fa235ef21a
Harenabs21 <hei.harena.6@gmail.com>
2025-08-22 15:56:43 +0300

    feat: Discovering and using ANSI codes to color text in the terminal

---
+++ day4_100/main.py
@@ -0,0 +1,18 @@
+print("Welcome to your Adventure Story Simulator.")
+print()
+print("I am going to ask you a bunch of questions and then create an epic story with you as the star.")
+
+print()
+name = input("What is your name? ")
+print()
+enemyName = input("What is your enemy's name? ")
+print()
+superPower = input("What is your super power? ")
+print()
+live = input("Where do you live? ")
+print()
+food = input("What is your favorite food? ")
+
+print()
+print("Hello", name, "Your ability to", "\033[36m", superPower, "\033[0m", "will make sure you never have to look at", "\033[31m", enemyName, "\033[0m", "again.", end=" ")
+print("Go eat", "\033[32m", food, "\033[0m", "as you walk down the streets of", live, "and use", "\033[36m", superPower, "\033[0m", "for good and not evil!")
